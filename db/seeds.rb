8af2c7ce3280990518f6f10fc86e017c51d76c85
Mirela Mustic <mirela.mustic00@gmail.com>
2024-02-20 19:54:21 +0000

    added resataurant and review models and controllers and the respective method definitions

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+pizza_east =  {name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'italian' }
+clos_maggiore = { name: 'Clos Maggiore', address: '33 King St, London WC2E 8JD', category: 'french' }
+koya_ko = { name: 'Koya Ko', address: '10 Broadway Market Mews London E8 4TS', category: 'japanese' }
+gloria = { name: 'Gloria', address: '54-56 Great Eastern St', category: 'italian' }
+hunan = { name: 'Hunan', address: '    51 Pimlico Road London SW1W 8NE', category: 'chinese' }
+
+[pizza_east, clos_maggiore, koya_ko, gloria, hunan].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
