ea639c95587190aa55fe23e765034f24de5808dc
Mirela Mustic <mirela.mustic00@gmail.com>
2024-05-10 16:13:42 +0100

    added review form to restaurant show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,19 +6,19 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review.restaurant = @restaurant
+    @review = @restaurant.reviews.build(review_params)
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new, status: :unprocessable_entity
+      render 'restaurants/show', status: :unprocessable_entity
     end
   end
+
 
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:rating, :content, :restaurant_id)
   end
 end
