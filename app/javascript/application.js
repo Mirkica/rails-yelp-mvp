4f94d0d9a778adcabe1b3ff471d47ee224efc4c5
Mirela Mustic <mirela.mustic00@gmail.com>
2024-05-10 16:23:01 +0100

    added an eventlistener on the form to toggle it when needed to add a review

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", function() {
+  const toggleButton = document.getElementById("toggle-review-form");
+  const reviewForm = document.getElementById("review-form");
+
+  toggleButton.addEventListener("click", function() {
+    if (reviewForm.style.display === "none") {
+      reviewForm.style.display = "block";
+    } else {
+      reviewForm.style.display = "none";
+    }
+  });
+});
