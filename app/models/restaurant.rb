abb2f64efe883385fed5bdb6caf93154e38dad73
Mirela Mustic <mirela.mustic00@gmail.com>
2024-05-10 14:04:31 +0100

    fixed bug with category validation for restaurant in model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, :address, uniqueness: true, presence: true
-  validates :category, presence: true, acceptance: { accept:['chinese', 'italian', 'japanese', 'french', 'belgian'] }
+  validates :category, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,11 @@ puts 'Cleaning database...'
 Restaurant.destroy_all
 
 puts 'Creating restaurants...'
-pizza_east =  {name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'italian' }
-clos_maggiore = { name: 'Clos Maggiore', address: '33 King St, London WC2E 8JD', category: 'french' }
-koya_ko = { name: 'Koya Ko', address: '10 Broadway Market Mews London E8 4TS', category: 'japanese' }
-gloria = { name: 'Gloria', address: '54-56 Great Eastern St', category: 'italian' }
-hunan = { name: 'Hunan', address: '    51 Pimlico Road London SW1W 8NE', category: 'chinese' }
+pizza_east =  {name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'Italian' }
+clos_maggiore = { name: 'Clos Maggiore', address: '33 King St, London WC2E 8JD', category: 'French' }
+koya_ko = { name: 'Koya Ko', address: '10 Broadway Market Mews London E8 4TS', category: 'Japanese' }
+gloria = { name: 'Gloria', address: '54-56 Great Eastern St', category: 'Italian' }
+hunan = { name: 'Hunan', address: '51 Pimlico Road London SW1W 8NE', category: 'Chinese' }
 
 [pizza_east, clos_maggiore, koya_ko, gloria, hunan].each do |attributes|
   restaurant = Restaurant.create!(attributes)
